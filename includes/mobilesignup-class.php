498d697346da06cef97d9dd830acab2be77d3c6e
Bhanu Singh <bhanukrsingh@gmail.com>
2020-04-30 13:24:00 +0530

    Fixed Push

--- includes/mobilesignup-class.php
+++ includes/mobilesignup-class.php
@@ -1,6 +1,62 @@
 <?php
 
-require_once(plugin_dir_path(__FILE__).'/includes/crushfitness_databaseCall.php');
+function parijatDatabaseCall($data){
+
+$url = "https://wallet.parijat.in/api_ms/app.php?rquest=".$data['rquest'];
+
+    $payloadName = json_encode($data);
+
+    //print_r($payloadName);
+
+    $headers = array('Content-Type:application/json');                         
+
+    $process = curl_init($url);
+
+    curl_setopt($process, CURLOPT_CUSTOMREQUEST, "POST");
+
+    curl_setopt($process, CURLOPT_POSTFIELDS, $payloadName);
+
+    curl_setopt($process, CURLOPT_RETURNTRANSFER, true);
+
+    curl_setopt($process, CURLOPT_HTTPHEADER,$headers);
+
+    curl_setopt($process, CURLOPT_SSL_VERIFYPEER, false);
+
+    curl_setopt($process, CURLOPT_TIMEOUT, 40);
+
+    $response=curl_exec($process);
+
+    //print_r($response);
+
+    if(!$response){
+
+                    return "Error In Data";
+
+    } else {
+
+                    return $response;
+
+    }
+
+     curl_close($process);
+
+ start_headers();
+
+    if(!$response){
+
+                    echo "Error 1";die;
+
+                    return false;
+
+    } else{
+
+                    echo "Done";
+
+                    return  $response;
+
+    }
+
+}
 
 
 add_action( 'wp_ajax_nopriv_send_otp_ajax_request', 'send_otp_to_the_number' );
@@ -8,36 +64,34 @@ add_action( 'wp_ajax_send_otp_ajax_request', 'send_otp_to_the_number' );  // For
 
 function send_otp_to_the_number() {
     
-    check_ajax_referer( 'aj-demo-nonce', 'nonce' );  // This function will die if nonce is not correct.
-	
-    
+    check_ajax_referer( 'aj-demo-nonce', 'nonce' ); 
+        
     $mobile_number = $_POST['data']['mob'];
-	
-	$mobile_number = (int)$mobile_number;
+    
+    $mobile_number = (int)$mobile_number;
     
     $otp =  rand(100000,999999);
 
     if(save_otp_inside_the_database($mobile_number, $otp)){
         
-        // Sending Message 
         $token = "dc8df2b565ada07c61cdb2a3e237bea1";
 
 
-    	$data=array(
+        $data=array(
 
-			"userid"=>"na",
+            "userid"=>"na",
 
-			"api_token"=> $token,
+            "api_token"=> $token,
 
-			" mobile "=> $mobile_number,
+            " mobile "=> $mobile_number,
 
-			"rquest"=>"sendSMS ",
+            "rquest"=>"sendSMS ",
 
-			"otp"=> $otp
+            "otp"=> $otp
 
-			);
+            );
 
-		$result = json_decode(parijatDatabaseCall($data),true);
+        $result = json_decode(parijatDatabaseCall($data),true);
 
 
         if( $result) {
@@ -54,7 +108,8 @@ function send_otp_to_the_number() {
                 array(
                     'mobile' => strval( $mobile_number ),
                     'sent' => false,
-                    'response' => $result
+                    'response' => $result,
+                    'otp' => $otp,
                 )
             );
         }
@@ -68,10 +123,10 @@ function save_otp_inside_the_database($mobile_number, $otp){
     global $wpdb;
     $table_name = $wpdb->prefix . "otp_table";
     $insert = $wpdb->insert($table_name, 
-    	array('number' => $mobile_number, 
-    		'otp' => $otp,
-    	    'otp_is_used' => '0'
-    	) ); 
+        array('number' => $mobile_number, 
+            'otp' => $otp,
+            'otp_is_used' => '0'
+        ) ); 
 
     if($insert) {
         return true;
@@ -98,7 +153,7 @@ function handleOtpVerificationFunction() {
     $table_name = $wpdb->prefix . "otp_table";
     
     $rows = $wpdb->get_row('SELECT * FROM ' . $table_name .' WHERE number ='.$mobile_number.' ORDER BY ID DESC', ARRAY_A);
-	
+    
     $otp_from_database = $rows['otp'];
     
     
